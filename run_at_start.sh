430f3f1ae21435a8e699a7963ddc65d1744139da
Brijesh Krishna <63464137+Brijeshkrishna@users.noreply.github.com>
2022-08-18 22:03:43 +0530

    Update run_at_start.sh

--- run_at_start.sh
+++ run_at_start.sh
@@ -1,1 +1,12 @@
-# run at start
+#info
+echo "\nhostname is `hostname -I` use ssh root@`hostname -I` to connect\n"
+
+# start commands go there
+##########################
+
+
+
+###########################
+
+# start sshd
+/usr/sbin/sshd -D -e
